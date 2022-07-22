5c0f691c5285f618ceb0df0dafc18bfe771e355d
m4rtintrujillo <m4rtin.trujillo@gmail.com>
2022-07-22 11:58:59 -0500

    left sidebar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Navbar from '../componentes/Navbar'
+import SidebarLeft from "../componentes/SidebarLeft"
+
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <SidebarLeft/>
     </div>
   )
 }
