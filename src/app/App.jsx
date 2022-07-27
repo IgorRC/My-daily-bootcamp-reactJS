f0c11d06732804d52d5e98ba2242b6f255182a97
sandro villegas <zony123rbd@gmail.com>
2022-07-27 18:01:35 -0500

    nueavas cosas

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,22 @@
-import './App.css'
-import Navbar from '../componentes/Navbar'
-import SidebarLeft from "./contenedores/SidebarLeft"
-import DailyContent from './contenedores/DailyContent'
-import SidebarRight from './contenedores/SidebarRight'
+import "./App.css";
+import Navbar from "../componentes/Navbar";
+import SidebarLeft from "./contenedores/SidebarLeft";
+import DailyContent from "./contenedores/DailyContent";
+import SidebarRight from "./contenedores/SidebarRight";
+import React, { useEffect } from "react";
+
 
 function App() {
-
   return (
     <div className="App">
-      <Navbar/>
-      <div className='main'>
-        <SidebarLeft/>
-        <DailyContent/>
-        <SidebarRight/>
+      <Navbar />
+      <div className="main">
+        <SidebarLeft />
+        <DailyContent />
+        <SidebarRight />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/contenedores/DailyContent.jsx
+++ src/app/contenedores/DailyContent.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import LearningContent from "../../componentes/LearningContent";
 import Public from "../../componentes/Public";
-import '../estilos-contenedores/DailyContent.css'
-
+import "../estilos-contenedores/DailyContent.css";
 
 function DailyContent() {
-
   return (
-  <div className="add-new-content">
-    <LearningContent/>
-    <Public/>
-  </div>
+    <div className="add-new-content">
+      <LearningContent />
+      <Public />
+    </div>
   );
 }
 
